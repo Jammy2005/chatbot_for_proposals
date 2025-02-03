02c4261934099cae77f966dbb255a6b8a6fff50b
Jammy2005 <134600717+Jammy2005@users.noreply.github.com>
2025-02-03 17:01:57 +0300

    exploring the MessageState how its manipulated included a full graph and its invokation

--- scripts/chain2.py
+++ scripts/chain2.py
@@ -8,48 +8,29 @@ from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph import StateGraph, START, END, MessagesState
 
-load_dotenv()
-    
-# model = OllamaLLM(model = "deepseek-r1:7b")
-model = ChatOpenAI(model="gpt-4o", temperature=0) 
-
-# define as tool
-def multiply(a: int, b: int) -> int:
-    """Multiply a and b.
-
-    Args:
-        a: first int
-        b: second int
-    """
-    return a * b
-
-# define model with tool
-model_with_tools = model.bind_tools([multiply])
 
 # using MessageState
 class MessageState(MessagesState):
     pass
 
-def tool_calling_model(state: MessagesState):
-    # print(state)
-    # print("XXXXXXXXXXXX")
-    # print({"messages": [model_with_tools.invoke(state["messages"])]})
-    # print("XXXXXXXXXXXX")
-    # print(state)
-    return {"messages": [model_with_tools.invoke(state["messages"])]}
+def dummy3(state: MessagesState):
+    print(state)
+    print("XXXXXXXXXXXX")
+    return {"messages": "I am"}
 
 def dummy(state: MessagesState):
-    # print(state)
-    # print("XXXXXXXXXXXX")
-    return {"messages": state["messages"]}
+    print(state)
+    print(state["messages"])
+    print("XXXXXXXXXXXX")
+    return {"messages": "CHAD"}
 
 def dummy2(state: MessagesState):
-    # print(state)
+    print(state)
     return {"messages": state["messages"]}
 
 # building graph
 builder = StateGraph(MessageState)
-builder.add_node("tool_calling_model", tool_calling_model)
+builder.add_node("tool_calling_model", dummy3)
 builder.add_node("dummy", dummy)
 builder.add_node("dummy2", dummy2)
 builder.add_edge(START, "tool_calling_model")
@@ -60,11 +41,12 @@ builder.add_edge("dummy2", END)
 
 graph = builder.compile()
 
-messages = graph.invoke({"messages": HumanMessage(content='2 multiply by 3')})
+messages = graph.invoke({"messages": HumanMessage(content='Hello')})
 
 for m in messages['messages']:
     # print(m.content)
-    m.pretty_print()
+    # m.pretty_print()
+    pass
 
 
 
